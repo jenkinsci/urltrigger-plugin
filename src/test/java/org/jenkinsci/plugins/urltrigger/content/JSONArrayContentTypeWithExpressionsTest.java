a9fc2e6261077c8d4e2fd2e31b64278e55f63857
Christian Lipphardt <christian.lipphardt@googlemail.com>
2015-11-23 16:50:30 +0100

    chore(json/array): allow to use jsonarray as root of input message

---
+++ src/test/java/org/jenkinsci/plugins/urltrigger/content/JSONArrayContentTypeWithExpressionsTest.java
@@ -0,0 +1,35 @@
+package org.jenkinsci.plugins.urltrigger.content;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
+public class JSONArrayContentTypeWithExpressionsTest extends AbstractJSONContentTypeTest {
+
+    public JSONArrayContentTypeWithExpressionsTest() {
+        JSONContentEntry[] expressions = new JSONContentEntry[]{new JSONContentEntry("$..name")};
+        type = new JSONContentType(Arrays.<JSONContentEntry>asList(expressions));
+    }
+
+    protected String getOldContentNotEmpty() throws IOException, URISyntaxException {
+        return readContentAsString("json/expressions/oldGitHubContent.json");
+    }
+
+    protected String getNewContent() throws IOException, URISyntaxException {
+        return readContentAsString("json/expressions/newGitHubContent.json");
+    }
+
+    @Test
+    public void testIsTriggeringMatchingNewContentWithEmptyPreviousContent() throws Exception {
+        Assert.assertTrue(isTriggeringBuildForContentWithChange_EmptyTypePreviousContent());
+    }
+
+    @Test
+    public void testIsTriggeringMatchingNewContentWithMatchingPreviousContent() throws Exception {
+        Assert.assertTrue(isTriggeringBuildForContentWithChange_AnyContentPreviousContent());
+    }
+}
+
