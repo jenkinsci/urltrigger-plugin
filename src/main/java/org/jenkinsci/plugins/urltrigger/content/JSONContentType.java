bdfb1be0ff97abf014341936ab90546bf00968ce
Kemal Soysal <Kemal.Soysal@ls-it-solutions.de>
2019-10-18 00:45:38 +0200

    JENKINS-59831 update pom.xml with json path 2.4.0
    remove unused ParseException,
    handle PathNotFoundException of json path for empty document
    when searching an empty document.
    add $..[?(@.category =~ /refer.+/)].title to test expressions

--- src/main/java/org/jenkinsci/plugins/urltrigger/content/JSONContentType.java
+++ src/main/java/org/jenkinsci/plugins/urltrigger/content/JSONContentType.java
@@ -1,7 +1,9 @@
 package org.jenkinsci.plugins.urltrigger.content;
 
-import com.jayway.jsonpath.JsonPath;
-import hudson.Extension;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.jenkinsci.Symbol;
 import org.jenkinsci.lib.xtrigger.XTriggerException;
@@ -9,11 +11,10 @@ import org.jenkinsci.lib.xtrigger.XTriggerLog;
 import org.jenkinsci.plugins.urltrigger.content.json.util.JsonUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
+
+import hudson.Extension;
 
 /**
  * @author Gregory Boissinot
@@ -24,120 +25,124 @@ public class JSONContentType extends URLTriggerContentType {
 
 	private transient Map<String, Object> results = null;
 
-    private List<JSONContentEntry> jsonPaths = new ArrayList<JSONContentEntry>();
+	private List<JSONContentEntry> jsonPaths = new ArrayList<JSONContentEntry>();
 
-    @DataBoundConstructor
-    public JSONContentType(List<JSONContentEntry> jsonPaths) {
-        if (jsonPaths != null) {
-            this.jsonPaths = jsonPaths;
-        }
-    }
+	@DataBoundConstructor
+	public JSONContentType(List<JSONContentEntry> jsonPaths) {
+		if (jsonPaths != null) {
+			this.jsonPaths = jsonPaths;
+		}
+	}
 
-    @SuppressWarnings("unused")
-    public List<JSONContentEntry> getJsonPaths() {
-        return jsonPaths;
-    }
+	@SuppressWarnings("unused")
+	public List<JSONContentEntry> getJsonPaths() {
+		return jsonPaths;
+	}
 
 	@Override
-    protected void initForContentType(String content, XTriggerLog log) throws XTriggerException {
+	protected void initForContentType(String content, XTriggerLog log) throws XTriggerException {
 		try {
 			JsonUtils.validateJson(content);
 			results = readJsonPath(content);
-		} catch( XTriggerException pe ) {
-			log.error( "An error occurred when parsing the document - may not be valid JSON?" ) ;
-			throw pe ;
+		} catch (XTriggerException pe) {
+			log.error("An error occurred when parsing the document - may not be valid JSON?");
+			throw pe;
 		}
-    }
+	}
 
-    private Map<String, Object> readJsonPath(String content) throws XTriggerException {
-        Map<String, Object> results = new HashMap<String, Object>(jsonPaths.size());
-        try {
-            for (JSONContentEntry jsonContentEntry : jsonPaths) {
-                String jsonPath = jsonContentEntry.getJsonPath();
-                Object result = JsonPath.read(content, jsonPath);
-                results.put(jsonPath, result);
-            }
-        } catch (ParseException pe) {
-            throw new XTriggerException(pe);
-        }
-        return results;
-    }
+	private Map<String, Object> readJsonPath(String content) throws XTriggerException {
+		Map<String, Object> results = new HashMap<String, Object>(jsonPaths.size());
+		for (JSONContentEntry jsonContentEntry : jsonPaths) {
+			String jsonPath = jsonContentEntry.getJsonPath();
+			try {
+				Object result = JsonPath.read(content, jsonPath);
+				results.put(jsonPath, result);
+			} catch (PathNotFoundException pe) {
+				results.put(jsonPath, "");
+				// result will be empty
+//            throw new XTriggerException(pe);
+			}
+		}
+		return results;
+	}
 
-    @Override
-    protected boolean isTriggeringBuildForContent(String content, XTriggerLog log) throws XTriggerException {
+	@Override
+	protected boolean isTriggeringBuildForContent(String content, XTriggerLog log) throws XTriggerException {
 
-        if (jsonPaths == null || jsonPaths.size() == 0) {
-            log.error("You must configure at least one JSON Path. Exit with no changes.");
-            return false;
-        }
+		if (jsonPaths == null || jsonPaths.size() == 0) {
+			log.error("You must configure at least one JSON Path. Exit with no changes.");
+			return false;
+		}
 
+		if (results == null) {
+			log.info("Capturing URL context. Waiting next schedule to check a change.");
+			return false;
+		}
 
-        if (results == null) {
-            log.info("Capturing URL context. Waiting next schedule to check a change.");
-            return false;
-        }
+		Map<String, Object> newResults = readJsonPath(content);
 
-        Map<String, Object> newResults = readJsonPath(content);
+		if (newResults == null) {
+			throw new NullPointerException("New computed results object must not be a null reference.");
+		}
 
-        if (newResults == null) {
-            throw new NullPointerException("New computed results object must not be a null reference.");
-        }
+		if (results.size() != newResults.size()) {
+			throw new XTriggerException("The size between old results and new results has to be the same.");
+		}
 
-        if (results.size() != newResults.size()) {
-            throw new XTriggerException("The size between old results and new results has to be the same.");
-        }
+		// The results object have to be the same keys
+		if (!results.keySet().containsAll(newResults.keySet())) {
+			throw new XTriggerException(
+					"Regarding the set up of the result objects, the keys for the old results and the new results have to be the same.");
+		}
 
-        //The results object have to be the same keys
-        if (!results.keySet().containsAll(newResults.keySet())) {
-            throw new XTriggerException("Regarding the set up of the result objects, the keys for the old results and the new results have to be the same.");
-        }
+		for (Map.Entry<String, Object> entry : results.entrySet()) {
 
-        for (Map.Entry<String, Object> entry : results.entrySet()) {
+			String jsonPath = entry.getKey();
+			Object initValue = entry.getValue();
+			Object newValue = newResults.get(jsonPath);
 
-            String jsonPath = entry.getKey();
-            Object initValue = entry.getValue();
-            Object newValue = newResults.get(jsonPath);
-            
-            boolean initValueNull = ( initValue == null );
-            boolean newValueNull = ( newValue == null );
+			boolean initValueNull = (initValue == null);
+			boolean newValueNull = (newValue == null);
 
-            if (initValueNull && newValueNull) {
-                log.info(String.format("There is no matching for the JSON Path '%s'.", jsonPath));
-                continue;
-            } else if (initValueNull && ! newValueNull) {
-                log.info(String.format("There was no value and now there is a new value for the JSON Path '%s'.", jsonPath));
-                return true;
-            } else if (! initValueNull && newValueNull) {
-                log.info(String.format("There was a value and now there is no value for the JSON Path '%s'.", jsonPath));
-                return true;
-            } else if (!initValue.equals(newValue)) {
-                log.info(String.format("The value for the JSON Path '%s' has changed.", jsonPath));
-                return true;
-            }
-        }
+			if (initValueNull && newValueNull) {
+				log.info(String.format("There is no matching for the JSON Path '%s'.", jsonPath));
+				continue;
+			} else if (initValueNull && !newValueNull) {
+				log.info(String.format("There was no value and now there is a new value for the JSON Path '%s'.",
+						jsonPath));
+				return true;
+			} else if (!initValueNull && newValueNull) {
+				log.info(
+						String.format("There was a value and now there is no value for the JSON Path '%s'.", jsonPath));
+				return true;
+			} else if (!initValue.equals(newValue)) {
+				log.info(String.format("The value for the JSON Path '%s' has changed.", jsonPath));
+				return true;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    @Extension
-    @SuppressWarnings("unused")
-    @Symbol( "JsonContent" )
-    public static class JSONContentDescriptor extends URLTriggerContentTypeDescriptor<XMLContentType> {
+	@Extension
+	@SuppressWarnings("unused")
+	@Symbol("JsonContent")
+	public static class JSONContentDescriptor extends URLTriggerContentTypeDescriptor<XMLContentType> {
 
-        @Override
-        public Class<? extends URLTriggerContentType> getType() {
-            return JSONContentType.class;
-        }
+		@Override
+		public Class<? extends URLTriggerContentType> getType() {
+			return JSONContentType.class;
+		}
 
-        @Override
-        public String getDisplayName() {
-            return "Monitor the contents of a JSON path";
-        }
+		@Override
+		public String getDisplayName() {
+			return "Monitor the contents of a JSON path";
+		}
 
-        @Override
-        public String getLabel() {
-            return "JSON";
-        }
-    }
+		@Override
+		public String getLabel() {
+			return "JSON";
+		}
+	}
 
 }
--- src/test/java/org/jenkinsci/plugins/urltrigger/content/JSONContentTypeWithExpressionsTest.java
+++ src/test/java/org/jenkinsci/plugins/urltrigger/content/JSONContentTypeWithExpressionsTest.java
@@ -1,38 +1,38 @@
 package org.jenkinsci.plugins.urltrigger.content;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * @author Gregory Boissinot
  */
 public class JSONContentTypeWithExpressionsTest extends AbstractJSONContentTypeTest {
 
-    public JSONContentTypeWithExpressionsTest() {
-        JSONContentEntry[] expressions = new JSONContentEntry[]{new JSONContentEntry("$.store.book[0].title")};
-        type = new JSONContentType(Arrays.<JSONContentEntry>asList(expressions));
-    }
+	public JSONContentTypeWithExpressionsTest() {
+		JSONContentEntry[] expressions = new JSONContentEntry[] { new JSONContentEntry("$.store.book[0].title"),
+				new JSONContentEntry("$..[?(@.category =~ /refer.+/)].title") };
+		type = new JSONContentType(Arrays.<JSONContentEntry>asList(expressions));
+	}
 
-    protected String getOldContentNotEmpty() throws IOException, URISyntaxException {
-        return readContentAsString("json/expressions/oldJsonContent.json");
-    }
+	protected String getOldContentNotEmpty() throws IOException, URISyntaxException {
+		return readContentAsString("json/expressions/oldJsonContent.json");
+	}
 
-    protected String getNewContent() throws IOException, URISyntaxException {
-        return readContentAsString("json/expressions/newJsonContent.json");
-    }
+	protected String getNewContent() throws IOException, URISyntaxException {
+		return readContentAsString("json/expressions/newJsonContent.json");
+	}
 
-    @Test
-    public void testIsTriggeringMatchingNewContentWithEmptyPreviousContent() throws Exception {
-        Assert.assertTrue(isTriggeringBuildForContentWithChange_EmptyTypePreviousContent());
-    }
+	@Test
+	public void testIsTriggeringMatchingNewContentWithEmptyPreviousContent() throws Exception {
+		Assert.assertTrue(isTriggeringBuildForContentWithChange_EmptyTypePreviousContent());
+	}
 
-    @Test
-    public void testIsTriggeringMatchingNewContentWithMatchingPreviousContent() throws Exception {
-        Assert.assertTrue(isTriggeringBuildForContentWithChange_AnyContentPreviousContent());
-    }
+	@Test
+	public void testIsTriggeringMatchingNewContentWithMatchingPreviousContent() throws Exception {
+		Assert.assertTrue(isTriggeringBuildForContentWithChange_AnyContentPreviousContent());
+	}
 }
-
